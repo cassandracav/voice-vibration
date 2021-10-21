effa5771c00133f5c39a17a02b85f901fa4161d0
Cassandra Cavenaugh <cassandracav@gmail.com>
2021-10-21 14:48:39 -0400

    Initial commit

---
+++ speech-recognition.js
@@ -0,0 +1,75 @@
+if ("webkitSpeechRecognition" in window) {
+  // Initialize webkitSpeechRecognition
+  let speechRecognition = new webkitSpeechRecognition();
+
+  // String for the Final Transcript
+  let final_transcript = "";
+
+  // Set the properties for the Speech Recognition object
+  speechRecognition.continuous = true;
+  speechRecognition.interimResults = true;
+  speechRecognition.lang = document.querySelector("#select_dialect").value;
+
+  // Callback Function for the onStart Event
+  speechRecognition.onstart = () => {
+    // Show the Status Element
+    document.querySelector("#status").style.display = "block";
+  };
+  speechRecognition.onerror = () => {
+    // Hide the Status Element
+    document.querySelector("#status").style.display = "none";
+  };
+  speechRecognition.onend = () => {
+    // Hide the Status Element
+    document.querySelector("#status").style.display = "none";
+  };
+
+  speechRecognition.onresult = (event) => {
+    // Create the interim transcript string locally because we don't want it to persist like final transcript
+    //let interim_transcript = "";
+
+    // Loop through the results from the speech recognition object.
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      // If the result item is Final, add it to Final Transcript
+      if (event.results[i].isFinal) {
+        final_transcript = event.results[i][0].transcript;
+      }
+    }
+
+    // Set the Final transcript and Interim transcript.
+    document.querySelector("#final").innerHTML = final_transcript;
+
+    // Check if the phrase is in the table, if so, execute
+    parse(final_transcript);
+    final_transcript = "";
+    console.log(final_transcript);
+  };
+
+  // Set the onClick property of the start button
+  document.querySelector("#start").onclick = () => {
+    // Start the Speech Recognition
+    speechRecognition.start();
+  };
+  // Set the onClick property of the stop button
+  document.querySelector("#stop").onclick = () => {
+    // Stop the Speech Recognition
+    speechRecognition.stop();
+  };
+} else {
+  console.log("Speech Recognition Not Available");
+}
+
+async function parse(phrase){
+
+    var foundIndex = null;
+    rewardArray.forEach((row, index) => {
+        console.log(row);
+        if(row[0].trim() == phrase.trim()){
+            foundIndex = index;
+        }
+    })
+    
+    if(!(foundIndex === null)){
+        await rewardVibration(rewardArray[foundIndex][1], rewardArray[foundIndex][2]);
+    }
+}
